9ef3599898f65f6a2fc514ff122012a210786bc2
AlexeiDeepIA <alexeipalacios@deepia.mx>
2024-03-31 20:21:09 -0600

    [fix] body parser

--- index.js
+++ index.js
@@ -5,24 +5,37 @@ const morgan = require('morgan');
 const verifyToken = require('./middlewares/ValidateToken.js');
 const bodyParser = require("body-parser");
 const UserRoutes = require("./routes/UserRoute.js");
-const CammpaignRoutes = require("./routes/CampignRoute.js");
-const Azure = require('./routes/AzureRoute.js')
+const CampaignRoutes = require ('./routes/CampignRoute.js');
+const AzureRoutes = require('./routes/AzureRoute.js');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-db.run();
+// Configuración de middleware
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: '*'
-}));
+app.use(bodyParser.json());
+app.use(morgan('combined')); // Puedes ajustar el formato de registro según tus necesidades
 
+// Rutas
 app.use("/api/users", UserRoutes);
-app.use("/api/campaigns", CammpaignRoutes)
-app.use('/api', Azure);
-app.use(verifyToken);
-app.use(bodyParser.json())
+app.use("/api/campaigns", CampaignRoutes);
+app.use('/api', AzureRoutes);
 
-app.listen(port, () => {
-    console.log(`Server Runnig on ${process.env.HOSTNAME}:${port}`);
+// Middleware de verificación de token
+app.use(verifyToken);
+
+// Middleware de manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Iniciar el servidor
+db.run().then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}).catch(err => {
+  console.error('Error connecting to database:', err.message);
 });
